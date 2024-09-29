b431b6761ec651d8dea16e993b8715c25787c504
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2024-09-29 14:02:11 +0300

    1

---
+++ StatelessTest/Tests/UnitTest1.cs
@@ -0,0 +1,91 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BugTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInitialState()
+        {
+            var bug = new Bug(Bug.State.Open);
+            Assert.AreEqual(Bug.State.Open, bug.getState());
+        }
+
+        [TestMethod]
+        public void TestAssignFromOpen()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.getState());
+        }
+
+        [TestMethod]
+        public void TestCloseFromAssigned()
+        {
+            var bug = new Bug(Bug.State.Assigned);
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.getState());
+        }
+
+        [TestMethod]
+        public void TestDeferFromAssigned()
+        {
+            var bug = new Bug(Bug.State.Assigned);
+            bug.Defer();
+            Assert.AreEqual(Bug.State.Defered, bug.getState());
+        }
+
+        [TestMethod]
+        public void TestReassignFromDefered()
+        {
+            var bug = new Bug(Bug.State.Defered);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.getState());
+        }
+
+        [TestMethod]
+        public void TestReassignFromClosed()
+        {
+            var bug = new Bug(Bug.State.Closed);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.getState());
+        }
+
+        [TestMethod]
+        public void TestIgnoreAssignInAssigned()
+        {
+            var bug = new Bug(Bug.State.Assigned);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.getState());
+        }
+
+        [TestMethod]
+        public void TestDeferWithoutAssign()
+        {
+            var bug = new Bug(Bug.State.Open);
+            Assert.ThrowsException<InvalidOperationException>(() => bug.Defer());
+        }
+
+        [TestMethod]
+        public void TestCloseWithoutAssign()
+        {
+            var bug = new Bug(Bug.State.Open);
+            Assert.ThrowsException<InvalidOperationException>(() => bug.Close());
+        }
+
+        [TestMethod]
+        public void TestFullFlow()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.getState());
+            bug.Defer();
+            Assert.AreEqual(Bug.State.Defered, bug.getState());
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.getState());
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.getState());
+        }
+    }
+}
